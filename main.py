d3022ac25a1ba2104d1382c9c21f1dd1a6924af5
alitehfe16 <95401468+alitehfe16@users.noreply.github.com>
2026-02-02 18:45:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import os
-from sqlalchemy import create_engine, text
 
 app = FastAPI()
-
-# conexión a Supabase (Railway ya tiene esta variable)
-DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
-
-class Producto(BaseModel):
-    nombre: str
-    precio: float
-    stock: int
 
 @app.get("/")
 def root():
     return {"status": "ok"}
-
-@app.post("/productos")
-def crear_producto(producto: Producto):
-    with engine.connect() as conn:
-        conn.execute(
-            text(
-                "INSERT INTO productos (nombre, precio, stock) "
-                "VALUES (:nombre, :precio, :stock)"
-            ),
-            {
-                "nombre": producto.nombre,
-                "precio": producto.precio,
-                "stock": producto.stock
-            }
-        )
-        conn.commit()
-
-    return {"mensaje": "Producto guardado"}
